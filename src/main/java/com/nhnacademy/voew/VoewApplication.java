f02ae0fc3533816b7cd5a6d9be7634c643a54cc7
채상희 <chaesanghui142@gmail.com>
2024-06-14 21:11:58 +0900

    first

---
+++ src/main/java/com/nhnacademy/voew/VoewApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.voew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoewApplication.class, args);
+    }
+
+}
